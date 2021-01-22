52449371981ffe9fbe95e2284b05e7730e29106c
barosanuemailtest <barosanuemailtest@gmail.com>
2021-01-22 16:30:16 +0200

    Cognito initial setup

--- src/login/LoginService.ts
+++ src/login/LoginService.ts
@@ -1,10 +1,31 @@
 import { Auth } from "aws-amplify";
+import Amplify from "aws-amplify";
+import * as config from "../../config.json";
+
+Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+  });
 
 export class LoginService {
 
     public async login(userName: string, password: string): Promise<boolean> {
         const user = await Auth.signIn(userName, password)
         return false;
+    }
 
+    public async signUp(userName: string, password: string, email: string): Promise<boolean> {
+        const signUpResponse = await Auth.signUp({
+            username: userName,
+            password: password,
+            attributes: {
+                email: email
+            }
+        });
+        return false;
     }
 }
--- test/login/LoginService.test.ts
+++ test/login/LoginService.test.ts
@@ -4,14 +4,24 @@ import { LoginService } from "../../src/login/LoginService";
 
 describe('LoginService test suite', () => {
 
-    let loginService:LoginService;
+    let loginService: LoginService;
 
-    beforeEach(()=>{
+    beforeEach(() => {
         loginService = new LoginService();
     });
 
-    test('initial test', async () => {
-        await loginService.login('asd', 'def');
-    }); 
+    test.skip('signUp test', async () => {
+        await loginService.signUp(
+            'sefu',
+            '1234567',
+            'xxx@gmail.com'
+        )
+    });
+    test('login', async () => {
+        await loginService.login(
+            'sefu',
+            '1234567'
+        )
+    })
 
 })
