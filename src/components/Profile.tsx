5240f84e8c99da01ec16b62d43f3dd467aa0140d
barosanuemailtest <barosanuemailtest@gmail.com>
2021-02-09 18:13:38 +0200

    Profile css

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,7 @@
 import { CognitoUser } from "@aws-amplify/auth";
 import { Component } from "react";
 import { Link } from "react-router-dom";
+import './Profile.css';
 
 interface IProfileProps {
     user: CognitoUser | undefined
@@ -14,16 +15,27 @@ export class Profile extends Component<IProfileProps, IProfileState>{
     render() {
         let profileSpace;
         if (this.props.user) {
-            profileSpace = <h1>Welcome {this.props.user.getUsername()}</h1>
+            profileSpace =
+                <div id='profile-space'>
+                    <img src="img.jpg" alt="John"/>
+                        <h1>Nadia </h1>
+                        <p className="title">CEO & Founder, Example</p>
+                        <p>Harvard University</p>
+                        <a href="#"><i className="fa fa-dribbble"></i></a>
+                        <a href="#"><i className="fa fa-twitter"></i></a>
+                        <a href="#"><i className="fa fa-linkedin"></i></a>
+                        <a href="#"><i className="fa fa-facebook"></i></a>
+                        <p><button>Contact</button></p>
+                </div>
         } else {
-            profileSpace = <div>
-                Please <Link to="/login">Login</Link>
-            </div>
-        }
+                        profileSpace = <div>
+                            Please <Link to="/login">Login</Link>
+                        </div>
+                    }
         return <div>
-            <h2>Hello to the profile page!</h2>
-            {profileSpace}
-        </div>
+                        <h2>Hello to the profile page!</h2>
+                        {profileSpace}
+                    </div>
     }
 
 }
